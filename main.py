570d653d42a42bdae6dee543f0fd7e7a9bea7147
GabrielDelcominette <150939579+GabrielDelcominette@users.noreply.github.com>
2025-03-22 22:08:32 +0100

    versions finales (j'espère) des fonctions read et remove epsilon

--- main.py
+++ main.py
@@ -145,44 +145,6 @@ def user_menu(auto_exists=False):
 def main():
     print("Debut du programe")
 
-    '''state_0 = State(0, entry=True, exit=True)
-    state_1 = State(1, entry=True)
-    state_2 = State(2, exit=True)
-
-    transition_0 = {"a" : [],
-                    "b" : [state_1, state_2]}
-
-    transition_1 = {"a": [state_0, state_2],
-                    "b": [state_2]}
-
-    transition_2 = {"a": [state_0],
-                    "b": []}
-
-    state_0.add_transition_dict(transition_0)
-    state_1.add_transition_dict(transition_1)
-    state_2.add_transition_dict(transition_2)
-
-    Automate_0 = Automata([state_0, state_1, state_2])
-    Automate_0.display_automate()
-
-    Automate_0.determinate()
-    Automate_0.display_automate()
-
-    Automate_0.complete_automate()
-    Automate_0.display_automate()
-
-    print(" ")
-    print("Automate initial : ")
-    print(" ")
-
-    Automate_0.standardise()
-    Automate_0.display_automate()
-
-    print(" ")
-    print("Automate complémentaire : ")
-    print(" ")'''
-
-
 
     for i in range(1,45):
         print("\n\n===========================================")
@@ -224,9 +186,6 @@ def main():
         del Automate_0
         del Automate_1
 
-    '''Automate_0 = Automata()
-    Automate_0.read_automata("7")
-    Automate_0.display_automate()'''
 
 
 # Press the green button in the gutter to run the script.
